4ebf532ccb11d481b04d2d4453a11089587270d0
izal14551 <erwin14551@gmail.com>
2026-07-28 21:00:51 +0700

    Refactor filter-periode layout and styling

--- resources/views/partials/filter-periode.blade.php
+++ resources/views/partials/filter-periode.blade.php
@@ -2,7 +2,8 @@
     <div class="dash-panel-body">
         <form method="GET" action="{{ $action }}">
             <div class="row align-items-end g-3">
-                <div class="col-12 col-md-5 col-lg-4">
+                {{-- INPUT PERIODE (KIRI) --}}
+                <div class="col-12 col-md-6 col-lg-4">
                     <label class="form-label fw-semibold">Pilih Periode</label>
                     <input
                         type="month"
@@ -12,19 +13,18 @@
                     >
                 </div>
 
-                <div class="col-6 col-md-3 col-lg-2">
-                    <button type="submit" class="btn btn-primary w-100">
+                {{-- TOMBOL TAMPILKAN & RESET (KANAN DI DESKTOP) --}}
+                <div class="col-12 col-md-auto ms-md-auto d-flex align-items-center gap-2">
+                    <button type="submit" class="btn btn-primary px-4 py-2 flex-fill flex-md-grow-0">
                         <i class="bi bi-search me-1"></i> Tampilkan
                     </button>
-                </div>
 
-                @if(!empty($showReset))
-                    <div class="col-6 col-md-3 col-lg-2">
-                        <a href="{{ $action }}" class="btn btn-secondary w-100">
+                    @if(!empty($showReset))
+                        <a href="{{ $action }}" class="btn btn-secondary px-4 py-2 flex-fill flex-md-grow-0">
                             <i class="bi bi-arrow-clockwise me-1"></i> Reset
                         </a>
-                    </div>
-                @endif
+                    @endif
+                </div>
             </div>
         </form>
     </div>
